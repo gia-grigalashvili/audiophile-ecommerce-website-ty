29eb9cbfdc9371758c509c21637af2d250a9c330
gia-grigalashvili <grigalagia07@gmail.com>
2024-07-05 20:49:13 +0400

    made tow div and write inforamtion

--- src/components/Personal.tsx
+++ src/components/Personal.tsx
@@ -1,6 +1,36 @@
-import React from "react";
-
+import styled from "styled-components";
+import Person from "/public/assets/shared/mobile/image-best-gear.jpg";
 function Personal() {
-  return <div>Personal</div>;
+  return (
+    <Maindiv>
+      <img src={Person} alt="" />
+      <Infromation>
+        <h1>Bringing you the best audio gear</h1>
+        <p>
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
+      </Infromation>
+    </Maindiv>
+  );
 }
+const Maindiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  padding: 20px;
+  img {
+    width: 327px;
+    height: 300px;
+  }
+`;
+const Infromation = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 export default Personal;
